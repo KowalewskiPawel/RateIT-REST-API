b58630c5e2cd4f82543b8de4bbb0b76eb6764a0c
KowalewskiPawel <pawelkowalewsk@gmail.com>
2021-06-24 12:40:33 +0300

    restritcted origins middleware added

--- app.js
+++ app.js
@@ -3,34 +3,38 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 
+const restrictOrigin = require("./middlewares/restrictOrigin");
+
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
-.then (() => {
+  })
+  .then(() => {
     console.log("Database connection established");
-})
-.catch((err) => {
-    console.error(`ERROR: ${err}`)
-});
+  })
+  .catch((err) => {
+    console.error(`ERROR: ${err}`);
+  });
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(restrictOrigin);
 
 app.use("/users", require("./routes/users"));
 
 app.get("/ping", (req, res) => {
-    return res.send({
-        error: false,
-        message: "Server is OK"
-    });
+  return res.send({
+    error: false,
+    message: "Server is OK",
+  });
 });
 
 app.listen(PORT, () => {
-    console.log(`Listening on PORT: ${PORT}`);
+  console.log(`Listening on PORT: ${PORT}`);
 });
-
